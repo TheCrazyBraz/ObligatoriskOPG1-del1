08ccefb7fd7bee40c283e1eb4bb3ac84bd10ea01
marcl <marcl@MARC-SKOLEPC>
2023-03-06 00:15:50 +0100

    Add project files.

---
+++ ObligatoriskOPG1/Car.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace ObligatoriskOPG1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? Plate { get; set; }
+
+        public void ValidateCarModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException("Model is NULL, please enter Model name.");
+            }
+            if (Model.Length < 4)
+            {
+                throw new ArgumentException("Model must be at least 4 characters long. Your model name attempt: " + Model);
+            }
+
+        }
+        public void ValidatePrice()
+        {
+            if (Price <= 0)
+            {
+                throw new ArgumentException("Price cant be negative or 0, your price: " + Price);
+            }
+        }
+        public void ValidatePlate()
+        {
+            if (Plate == null)
+            {
+                throw new ArgumentNullException("Plate is NULL, please enter Plate ID.");
+            }
+            if (Plate.Length< 2 || Plate.Length > 7)
+            {
+                throw new ArgumentException("Plate must be between 2 and 7 characters, your plate: " + Plate);
+            }
+           
+        }
+        public void Validate()
+        {
+            ValidateCarModel();
+            ValidatePrice();
+            ValidatePlate();
+        }
+        public override string ToString()
+        {
+            return "ID: " + Id + ", Model: " + Model + ", Price: " + Price + ", Plate: " + Plate;
+        }
+    }
+
+}
---
+++ ObligatoriskOPG1Tests/CarTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOPG1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ObligatoriskOPG1.Tests
+{
+    
+    [TestClass()]
+    public class CarTests
+    {
+        private Car CarSuccess = new Car { Id = 1, Model = "Ford Focus", Price = 230000, Plate = "XYZ123" };
+        private Car CarShortModel = new Car { Id = 2, Model = "Kia", Price = 212000, Plate = "XYZ123" };
+        private Car CarNullModel = new Car { Id = 3, Model = null, Price = 212000, Plate = "XYZ123" };
+        private Car CarBrokenPrice = new Car { Id = 4, Model = "Kia Picanto", Price = -12, Plate = "XYZ123" };
+        private Car CarShortPlate = new Car { Id = 5, Model = "BMW I3", Price = 650000, Plate = "A" };
+        private Car CarLongPlate = new Car { Id = 6, Model = "Mercedes Jokester", Price = 950000, Plate = "ABCDEFGHIJKL" };
+        private Car CarNullPlate = new Car { Id = 7, Model = "Mini Cooper", Price = 550000, Plate = null };
+
+        [TestMethod()]
+        public void ValidateCarModelTest()
+        {
+            CarSuccess.ValidateCarModel();
+            Assert.ThrowsException<ArgumentException>(() => CarShortModel.ValidateCarModel());
+            Assert.ThrowsException<ArgumentNullException>(() => CarNullModel.ValidateCarModel());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            CarSuccess.ValidatePrice();
+            Assert.ThrowsException<ArgumentException>(() => CarBrokenPrice.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidatePlateTest()
+        {
+            CarSuccess.ValidatePlate();
+            Assert.ThrowsException<ArgumentException>(() => CarShortPlate.ValidatePlate());
+            Assert.ThrowsException<ArgumentException>(() => CarLongPlate.ValidatePlate());
+            Assert.ThrowsException<ArgumentNullException>(() => CarNullPlate.ValidatePlate());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            CarSuccess.Validate();
+        }
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = CarSuccess.ToString();   // act
+            Assert.AreEqual("ID: 1, Model: Ford Focus, Price: 230000, Plate: XYZ123", str);  // assert
+        }
+    }
+}
